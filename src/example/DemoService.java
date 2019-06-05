4910d9fa8bb0de4cd6d7229941ce02a964d93819
alessanderleite <alessander.logistica@gmail.com>
2019-06-05 18:44:53 -0300

    code the DemoService class

--- src/example/DemoService.java
+++ src/example/DemoService.java
@@ -1,5 +1,18 @@
 package example;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/DemoService")
 public class DemoService {
 
+	@GET
+	@Path("/{msg}")
+	@Produces(MediaType.TEXT_HTML)
+	public String printMsg(@PathParam("msg") String msg) {
+		return "<h1>Your message: " +msg+ "</h1>";
+	}
 }
